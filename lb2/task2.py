4086dfe4d31f85b5bef62e898da5b42620dd2ef0
Arkadii <arkadiimomot@gmail.com>
2022-02-20 21:02:13 +0200

    Меморизация

--- lb2/task2.py
+++ lb2/task2.py
@@ -2,18 +2,17 @@ import sys
 sys.setrecursionlimit(100000)
     
      
-def ackermann_function(m, n):
-    memo = {}
+def ackermann_function(m, n,memo):
     if not (m, n) in memo:
         if m == 0:
             res = n + 1
         if m>0 and n==0:
-            res = ackermann_function(m-1,1)
+            res = ackermann_function(m-1,1,memo)
         if m>0 and n>0:
-            res = ackermann_function(m-1,ackermann_function(m,n-1))
+            res = ackermann_function(m-1,ackermann_function(m,n-1,memo),memo)
         memo[(m, n)] = res
     return memo[(m, n)]
      
 
-
-print(str(ackermann_function(3,7)))
+memo = {}
+print(str(ackermann_function(3,7,memo)))
