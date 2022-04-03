62913a599fafeeaf6fc251bd55271cf8aa745148
SophiaShchur <77302905+SophiaShchur@users.noreply.github.com>
2022-04-03 21:34:20 +0300

    Update main.py

--- lb4/main.py
+++ lb4/main.py
@@ -3,6 +3,28 @@ from InsertionSort import insertion_sort
 from SelectionSort import selection_sort 
 
 import numpy as np
+import copy
+
+
+arr = []
+
+def make_random_arr(n):
+    for i in range(n):
+        arr.append(np.random.randint(n))
+    return arr
+
+rand_1k = make_random_arr(1000)
+rand_10k = make_random_arr(10000)
+rand_100k = make_random_arr(100000)
+
+def make_arr(n):
+    arr = np.arange(n)
+    return arr
+
+arr_1k = make_arr(1000)
+arr_10k = make_arr(10000)
+arr_100k = make_arr(100000)
+
 
 bubble_sort(copy.copy(rand_1k))
 bubble_sort(copy.copy(rand_10k))
@@ -27,26 +49,6 @@ selection_sort(copy.copy(rand_100k))
 selection_sort(copy.copy(arr_1k))
 selection_sort(copy.copy(arr_10k))
 selection_sort(copy.copy(arr_100k))
-
-
-arr = []
-
-def make_random_arr(n):
-    for i in range(n):
-        arr.append(np.random.randint(n))
-    return arr
-
-rand_1k = make_random_arr(1000)
-rand_10k = make_random_arr(10000)
-rand_100k = make_random_arr(100000)
-
-def make_arr(n):
-    arr = np.arange(n)
-    return arr
-
-arr_1k = make_arr(1000)
-arr_10k = make_arr(10000)
-arr_100k = make_arr(100000)
 
 #def make_random_uniq_arr(n):
 #    arr = np.arange(n)
