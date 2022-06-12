54b5f5b4b6f0f4a55a486850c102c4593a9ceaa8
Arkadii <arkadiimomot@gmail.com>
2022-06-12 23:28:42 +0300

    Update GreedyAlgorithm№1.py

--- lb9/GreedyAlgorithm№1.py
+++ lb9/GreedyAlgorithm№1.py
@@ -1,29 +1,27 @@
+from pickle import NONE
 import numpy as np
 
 P = [1,2,3,4,5]
 C = [2,5,8,9,6]
 item = np.array(list(zip(P,C)))
-V = [4,5,2]
+V = [8,5,6]
 
 def Resource(item):
 	return np.lexsort([-1*item[:,1], item[:,0]])
 
 def VirtualMachine(item, V, index):
 	number = len(item)
-	status = [0] * number
+	status = [0] * len(V)
 	for i in range(number):
 		for j in range(len(V)):
 			if item[index[i],1] == V[j]:
-				status[index[i]] = item[index[i],0]
-				V.remove(V[j])
+				status[index[j]] = item[index[i],0]
 				break
-	if len(V) != 0:
-		for i in range(number):
-			for j in range(len(V)):
-				if status[i] == 0 and item[index[i],1] > V[j]:
-					status[index[i]] = item[index[i],0]
-					V.remove(V[j])
-					break
+	for i in range(number):
+		for j in range(len(V)):
+			if status[j] != 0 and status[j] != item[index[i],0] and item[index[i],1] > V[j]:
+				status[index[j]] = item[index[i],0]
+				break
 	return status
 
 print(item)
