47f950179a5bdfaa0a7dc7d74c587fa87df4e44d
Arkadii <arkadiimomot@gmail.com>
2022-06-12 21:59:33 +0300

    Update GreedyAlgorithm№1.py

--- lb9/GreedyAlgorithm№1.py
+++ lb9/GreedyAlgorithm№1.py
@@ -1,48 +1,32 @@
 import numpy as np
 
-weight = [35, 10 ,30, 60, 35, 25, 50, 30, 40, 10, 25, 45]
-price = [10, 5 ,40, 30, 45, 65, 50, 25, 35, 60, 30, 15]
-item = np.array(list(zip(weight,price)))
-S = 170
+P = [1,2,3,4,5]
+C = [2,5,8,9,6]
+item = np.array(list(zip(P,C)))
+V = [4,5,2]
 
-def Weight(item):
+def Resource(item):
 	return np.lexsort([-1*item[:,1], item[:,0]])
 
-def Price(item):
-	return np.lexsort([item[:,0], -1*item[:,1]])
-
-def Density(item):
-	number = len(item)
-	data_list = [0] * number
-	for i in range(number):
-		data_list[i] = (item[i,1])/(item[i,0])
-	data_set = np.array(data_list)
-	index = np.argsort(-1*data_set)
-	return index
-
-def Knapsack(item, S, index):
+def Knapsack(item, V, index):
 	number = len(item)
 	status = [0] * number
-	total_weight = 0
-	total_value = 0
 	for i in range(number):
-		if item[index[i],0] <= S:
-			total_weight += item[index[i],0]
-			total_value += item[index[i],1]
-			status[index[i]] = 1
-			S -= item[index[i],0]
-	return total_weight, total_value, status
+		for j in range(len(V)):
+			if item[index[i],1] == V[j]:
+				status[index[i]] = 1
+				V.remove(V[j])
+				break
+	if len(V) != 0:
+		for i in range(number):
+			for j in range(len(V)):
+				if status[i] == 0 and item[index[i],1] > V[j]:
+					status[index[i]] = 1
+					V.remove(V[j])
+					break
+	return status
 
 print(item)
-index_weight = Weight(item)
-index_price = Price(item)
-index_density = Density(item)
-results_weight = Knapsack(item, S, index_weight)
-print("Максимальна кількість предметів")
-print(results_weight)
-results_Price = Knapsack(item, S, index_price)
-print("Максимальна сумарна вартість")
-print(results_Price)
-print("Щось середнє")
-results_Density = Knapsack(item, S, index_density)
-print(results_Density)
+index_resource = Resource(item)
+results_resource = Knapsack(item, V, index_resource)
+print(results_resource)
